7b126894901f0e5e6c0cb213a7f7cd64bbadd99f
nikita.efimov <nikita.efimov@getgrid.ai>
2025-11-01 11:23:11 +0100

    Fix plugin to use standard File methods and improve error handling for GitHub Actions compatibility

--- _plugins/latest_revision_filter.rb
+++ _plugins/latest_revision_filter.rb
@@ -1,30 +1,41 @@
 # Jekyll plugin to filter daily_pages to only latest revision
 module Jekyll
   Hooks.register :site, :after_init do |site|
-    # Get all daily_pages directories
-    daily_pages_dir = Pathname.new(site.source).join("daily_pages")
-    next unless daily_pages_dir.directory?
-    
-    # Find latest revision directory
-    revision_dirs = daily_pages_dir.children.select { |d| d.directory? && d.basename.to_s.start_with?("content-") }
-    next if revision_dirs.empty?
-    
-    latest_dir = revision_dirs.sort_by { |d| d.basename.to_s }.last
-    latest_dir_name = latest_dir.basename.to_s
-    
-    # Exclude all other revision directories
-    revision_dirs.each do |dir|
-      next if dir == latest_dir
-      dir_name = dir.basename.to_s
-      exclude_path = "daily_pages/#{dir_name}"
-      exclude_path_pattern = "daily_pages/#{dir_name}/**"
-      site.exclude << exclude_path unless site.exclude.include?(exclude_path)
-      site.exclude << exclude_path_pattern unless site.exclude.include?(exclude_path_pattern)
+    begin
+      # Get all daily_pages directories
+      daily_pages_path = File.join(site.source, "daily_pages")
+      unless File.directory?(daily_pages_path)
+        Jekyll.logger.warn "Plugin:", "daily_pages directory not found at #{daily_pages_path}"
+        next
+      end
+      
+      # Find latest revision directory
+      revision_dirs = Dir.glob(File.join(daily_pages_path, "content-*")).select { |d| File.directory?(d) }
+      if revision_dirs.empty?
+        Jekyll.logger.warn "Plugin:", "No revision directories found in daily_pages"
+        next
+      end
+      
+      latest_dir = revision_dirs.sort_by { |d| File.basename(d) }.last
+      latest_dir_name = File.basename(latest_dir)
+      
+      # Exclude all other revision directories
+      revision_dirs.each do |dir|
+        next if dir == latest_dir
+        dir_name = File.basename(dir)
+        exclude_path = "daily_pages/#{dir_name}"
+        exclude_path_pattern = "daily_pages/#{dir_name}/**"
+        site.exclude << exclude_path unless site.exclude.include?(exclude_path)
+        site.exclude << exclude_path_pattern unless site.exclude.include?(exclude_path_pattern)
+      end
+      
+      Jekyll.logger.info "Plugin:", "Latest revision: #{latest_dir_name}"
+      excluded = (revision_dirs - [latest_dir]).map { |d| File.basename(d) }
+      Jekyll.logger.info "Plugin:", "Excluded revisions: #{excluded.join(', ')}" unless excluded.empty?
+    rescue => e
+      Jekyll.logger.error "Plugin Error:", e.message
+      Jekyll.logger.error "Plugin Error:", e.backtrace.join("\n")
     end
-    
-    Jekyll.logger.info "Latest revision:", latest_dir_name
-    excluded = (revision_dirs - [latest_dir]).map { |d| d.basename.to_s }
-    Jekyll.logger.info "Excluded revisions:", excluded.join(", ") unless excluded.empty?
   end
 end
 
